cd731a7fb1109346908eda14f41f766eb468e21b
Kksrlr03 <komalsriram000@gmail.com>
2026-09-08 22:26:07 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.SITE_CONFIG = {"businessName":"WEB DESIGNS","domain":"","email":"","whatsapp":"","formEnabled":false,"turnstileSiteKey":""};
+window.SITE_CONFIG = {"businessName":"WEB DESIGNS","domain":"https://webdesignss.com","email":"webdesignss.site@gmail.com","whatsapp":"918019480893","formEnabled":false,"turnstileSiteKey":""};
